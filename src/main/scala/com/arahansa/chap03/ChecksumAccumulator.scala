c75adddb1f144bab526521a692147a7d68a52ea4
arahansa <arahansa@naver.com>
2020-09-17 23:01:56 +0900

    refactoring - page 108

--- src/main/scala/com/arahansa/chap03/ChecksumAccumulator.scala
+++ src/main/scala/com/arahansa/chap03/ChecksumAccumulator.scala
@@ -3,11 +3,9 @@ package com.arahansa.chap03
 class ChecksumAccumulator {
   private var sum =0
 
-  def add(b:Byte) : Unit = {
-    sum += b
-  }
-  def checksum(): Int = {
-    return ~(sum & 0xFF) +1
-  }
+  def add(b:Byte) { sum += b }
+
+  def checksum(): Int = ~(sum & 0xFF) +1
+
 
 }
