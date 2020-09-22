f98d5f8aede502b6f65cf9ccd8891d6a24065506
arahansa <arahansa@naver.com>
2020-09-22 23:54:56 +0900

    add private field, method, chap06 - 150page

--- src/main/scala/com/arahansa/chap06/Rational.scala
+++ src/main/scala/com/arahansa/chap06/Rational.scala
@@ -2,8 +2,12 @@ package com.arahansa.chap06
 
 class Rational(n:Int, d:Int) {
   require(d!=0)
+  private val g = gcd(n.abs, d.abs)
   val number: Int = n
   val denom : Int = d
+
+  def this(n : Int) = this(n,1) // 보조 생성자
+
   override def toString = number +"/" + denom
 
   def add(that: Rational) : Rational =
@@ -14,5 +18,5 @@ class Rational(n:Int, d:Int) {
 
   def max(that : Rational) = if(this.lessThan(that)) that else this
 
-
+  private def gcd(a:Int, b:Int) : Int = if(b==0) a else gcd(b, a%b)
 }
